e2409f424889e40f571593f9b7f3dd22ae22443b
green <jonathangreen07@gmail.com>
2014-08-04 10:58:38 +0100

    Updated euler004.c

--- C/src/monte_carlo_pi.c
+++ C/src/monte_carlo_pi.c
@@ -4,18 +4,12 @@
 
 //Determine an approximate value for pi.
 
-#define LINE "_____________________________"
-#define TITLE "Monte Carlo: Pi"
-#define AUTHOR "Green"
-#define PROB "Calculate the value of Pi \n \t and associated error using a \n \t monte carlo numerical method."
 #define NUM_ITER 300000000
 #define SIDE 10000
 #define M_PI 3.14159265358979323846
 
 int main(int argc, char const *argv[])
 {
-	printf("\n \t %s \n \t %s \n \t %s \n \t %s \n", LINE, TITLE, AUTHOR, LINE);
-	printf("\t %s \n \t %s \n", PROB, LINE);	
 	//consider a unit square with a quarter circle
 
 	//declare variables
@@ -36,11 +30,11 @@ int main(int argc, char const *argv[])
 
 	//calculate pi
 	pi = 4*ratio/NUM_ITER;
-	printf("\t Pi: %lf\n", pi);
+	printf("Pi: %lf\n", pi);
 
 	//determine error
 	error = (M_PI-pi)*100./M_PI;
-	printf("\t Error: %lf%%\n\n", error);
+	printf("Error: %lf%%\n\n", error);
 
 	return 0;
 }
